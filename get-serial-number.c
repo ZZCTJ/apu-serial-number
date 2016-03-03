c80aa4379c0e81d2e106ed8394b6c63cf4e5381b
Andrew Cooks <acooks@linux.com>
2016-03-03 18:05:31 +0800

    fix off-by-one in byte position to integer conversion

--- get-serial-number.c
+++ get-serial-number.c
@@ -27,17 +27,13 @@ int main()
 		unsigned char mac_address[6];
 		unsigned char oui[3];
 		unsigned char sn[4] = {0};
-		int si = 0;
+		unsigned int si = 0;
 
 		memcpy(mac_address, ifr.ifr_hwaddr.sa_data, 6);
 		memcpy(oui, mac_address, 3);
 		memcpy(sn, &mac_address[3], 3);
 
-//		printf("sn: %02x:%02x:%02x:%02x\n", sn[0], sn[1], sn[2], sn[3]);
-
-//		printf("%s, oui: %02x:%02x:%02x\n", ifr.ifr_name, oui[0], oui[1], oui[2]);
-
-		si = sn[2] + 0xff * sn[1] + 0xffff * sn[0];
+		si = sn[2] + 256 * sn[1] + 65536 * sn[0];
 		si = si / 4 - 64;
 		printf("%d\n", si);
 	}
